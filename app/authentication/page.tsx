0d04703ce024a3a68cc50f190c4fa8259191bf48
Grace Lei <higracelei@gmail.com>
2023-12-11 09:35:27 -0500

    wip

--- app/authentication/page.tsx
+++ app/authentication/page.tsx
@@ -55,7 +55,7 @@ export default function AuthenticationPage() {
             >
               <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
             </svg>
-            优来斯
+            优来斯 Unice
           </div>
           <div className="relative z-20 mt-auto">
             <blockquote className="space-y-2">
@@ -70,7 +70,7 @@ export default function AuthenticationPage() {
           <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
             <div className="flex flex-col space-y-2 text-center">
               <h1 className="text-2xl font-semibold tracking-tight">
-                创建账号
+                Create an account
               </h1>
               <p className="text-sm text-muted-foreground">
                 Enter your email below to create your account
